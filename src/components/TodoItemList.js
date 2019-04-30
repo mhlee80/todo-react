51d8cd86adc808d33a397b782c34ad7ccd017edc
mhlee80 <myeonghyeon.lee@gmail.com>
2019-04-30 15:54:20 +0900

    TodoItemList에서 TodoItem생성시 color를 전달

--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -5,16 +5,17 @@ class TodoItemList extends Component {
   shouldComponentUpdate(nextProps, nextState) {
     return this.props.todos !== nextProps.todos;
   }
-  
+
   render() {
     const { todos, onToggle, onRemove } = this.props;
 
     const todoList = todos.map(
-      ({id, text, checked}) => (
+      ({id, text, checked, color}) => (
         <TodoItem
           id={id}
           text={text}
           checked={checked}
+          color={color}
           onToggle={onToggle}
           onRemove={onRemove}
           key={id}
